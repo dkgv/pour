b7bfa99d916ac52fc9e9af1155299c5b67ffc2a7
Gustav <gustav-git@pm.me>
2023-07-06 18:43:12 +0200

    Update database URL env variable

--- template/app/app/__init__.py
+++ template/app/app/__init__.py
@@ -8,7 +8,7 @@ def create_app() -> Flask:
     load_dotenv()
 
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 
     with app.app_context():
         from app.blueprints import register_blueprints
